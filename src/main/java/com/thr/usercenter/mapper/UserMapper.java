cbbd49844276cb21be11a39dbb10cbc34dbaf675
thr <thr@thr.com>
2024-01-29 11:55:47 +0800

    Initial commit

---
+++ src/main/java/com/thr/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.thr.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.thr.usercenter.model.User;
+import org.apache.ibatis.annotations.Mapper;
+
+public interface UserMapper extends BaseMapper<User> {
+}
