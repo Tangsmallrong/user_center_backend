dbba43f658e03534e88ec5589456cf46128957ab
thr <thr@thr.com>
2024-01-30 18:32:36 +0800

    数据库表设计

--- src/main/java/com/thr/usercenter/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.thr.usercenter.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.thr.usercenter.model.User;
-import org.apache.ibatis.annotations.Mapper;
-
-public interface UserMapper extends BaseMapper<User> {
-}
